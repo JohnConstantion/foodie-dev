5c21734b8d186ff18b03bb270d6eb1fc62d911f7
chenxianjun_ext <chenxianjun_ext@cop.cmsoft.com.cn>
2022-10-12 10:05:21 +0800

    add Application

---
+++ foodie-api/src/main/java/com/john/Application.java
@@ -0,0 +1,16 @@
+package com.john;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author johnconstantine
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
